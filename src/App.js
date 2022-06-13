5eac33c573b038cebb952393590f14889a849557
Dwarikanath1502 <ddwarikaanth15@gmail.com>
2022-06-13 21:47:45 +0530

    added grid to the applicaton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import Icon from './Components/Icon';
+import React, { useState } from "react";
+import "./App.css";
+import Icon from "./Components/Icon";
+//Toastify
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// Bootstrap
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  //TODO: methods
+  const reloadGame = () => {
+    //
+  };
+
+  const checkIsWinner = () => {
+    //
+  };
+
+  const changeItem = (iteNumber) => {
+    //
+  };
+
   return (
-   <div>
-    <Icon/>
-   </div>
+    <div>
+      <Container className="p-5">
+        <ToastContainer position="bottom-center" />
+        <Row>
+          <Col md={6} className="offset-md-3">
+            <div className="grid">
+              {itemArray.map((item, index) => (
+                <Card>
+                  <CardBody className="box">
+                    <Icon name={item} />
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
-}
+};
 
 export default App;
